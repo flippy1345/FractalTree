451fd2754348046d0b0b6f7b2b41a84f30d5f792
flippy1345 <flippy1345@gmail.com>
2017-09-28 17:41:50 +0200

    fixed color (kinda) added bend funtion

--- main.lua
+++ main.lua
@@ -1,27 +1,29 @@
 local screenWidth, screenHeight = 0, 0
 local center = {}
 local growLenght = 0
+local bend = 0
 
 function map(currentRange, minRange, maxRange, minReturnRange, maxReturnRange)
   return (currentRange * ((maxReturnRange - minReturnRange) / (maxRange - minRange)))
 end
 
-function branch(startPosX, startPosY, lenght, startAngle)
+function branch(startPosX, startPosY, lenght, startAngle, bend)
   if lenght > 2 then
     local rad = math.rad(startAngle)
     local endPosX = startPosX + lenght * math.cos(rad)
     local endPosY = startPosY + lenght * math.sin(rad)
     local color = {
-      ['r'] = map(startPosY, center.height + 250, 250, 0, 200),
-      ['g'] = map(lenght, 2, 100, 0, 0),
-      ['b'] = map(startPosX, 0, screenWidth, 0, 0)
+      ['r'] = map(startPosX, center.width-200, center.width+200, 0, 255),
+      ['g'] = 0,
+      ['b'] = map(100-lenght, 0, 100, 0, 100)
     }
+    --print("color.r: "..color.r)
     love.graphics.setColor(color.r, color.g, color.b)
     love.graphics.line(startPosX, startPosY, endPosX, endPosY)
     startAngle = startAngle + 35
     lenght = lenght - (lenght * 0.3)
-    branch(endPosX, endPosY, lenght, startAngle)
-    branch(endPosX, endPosY, lenght, startAngle - 70)
+    branch(endPosX, endPosY, lenght, startAngle, bend)
+    branch(endPosX, endPosY, lenght, startAngle - 70 + bend, bend)
   end
 end
 
@@ -30,16 +32,22 @@ function love.load(arg)
 end
 
 function love.update(dt)
+  deltaTime = dt
   growLenght = growLenght + dt * 4
-  if growLenght >= 100 then
-    growLenght = 100
+  if growLenght >= 200 then
+    growLenght = 200
   end
   love.window.setTitle("FractalTree - "..love.timer.getFPS().."FPS")
   screenWidth, screenHeight = love.graphics.getDimensions()
   center = {['width'] = screenWidth * 0.5, ['height'] = screenHeight * 0.5 }
-  print("mouseX: "..love.mouse.getX(), "map: "..map(love.mouse.getX(), 0, screenWidth, 0, 255))
+  --print("mouseX: "..love.mouse.getX(), "map: "..map(love.mouse.getX(), 0, screenWidth, 0, 255))
+end
+function love.keypressed(key, scancode, isrepeat)
+  if key == 'right' then
+    bend = bend+deltaTime*20
+  end
 end
 
 function love.draw()
-  branch(center.width, center.height + 250, 100, - 90)
+  branch(center.width, center.height + 250, 100, - 90, bend)
 end
