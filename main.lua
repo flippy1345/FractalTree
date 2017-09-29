3536d185e66bf8e36008477d5725151d68c2963a
flippy1345 <flippy1345@gmail.com>
2017-09-29 16:46:59 +0200

    added controlls and fixed color

--- main.lua
+++ main.lua
@@ -7,47 +7,77 @@ function map(currentRange, minRange, maxRange, minReturnRange, maxReturnRange)
   return (currentRange * ((maxReturnRange - minReturnRange) / (maxRange - minRange)))
 end
 
-function branch(startPosX, startPosY, lenght, startAngle, bend)
+function branch(startPosX, startPosY, lenght, startAngle, bend, side)
   if lenght > 2 then
     local rad = math.rad(startAngle)
     local endPosX = startPosX + lenght * math.cos(rad)
     local endPosY = startPosY + lenght * math.sin(rad)
     local color = {
-      ['r'] = map(startPosX, center.width-200, center.width+200, 0, 255),
-      ['g'] = 0,
-      ['b'] = map(100-lenght, 0, 100, 0, 100)
+      ['r'] = map(startPosX, center.width - startPosX, center.width + startPosX, 0, 255),
+      ['g'] = map(startPosY, center.height - startPosY, center.height + startPosY, 0, 20),
+      ['b'] = map(100 - lenght, 0, 100, 0, 150)
     }
-    --print("color.r: "..color.r)
     love.graphics.setColor(color.r, color.g, color.b)
+    love.graphics.setLineWidth(lenght * 0.1)
     love.graphics.line(startPosX, startPosY, endPosX, endPosY)
     startAngle = startAngle + 35
     lenght = lenght - (lenght * 0.3)
-    branch(endPosX, endPosY, lenght, startAngle, bend)
-    branch(endPosX, endPosY, lenght, startAngle - 70 + bend, bend)
+    branch(endPosX, endPosY, lenght, startAngle + bend, bend)-- right
+    branch(endPosX, endPosY, lenght, startAngle - 70 + bend, bend)-- left
+    --print()
+    --fractalWidthStart = center.width - 200
+    --fractalWidthEnd =  center.width + startPosX
   end
 end
 
 function love.load(arg)
-
+  love.keyboard.setKeyRepeat(true)
 end
 
 function love.update(dt)
   deltaTime = dt
-  growLenght = growLenght + dt * 4
-  if growLenght >= 200 then
-    growLenght = 200
-  end
   love.window.setTitle("FractalTree - "..love.timer.getFPS().."FPS")
   screenWidth, screenHeight = love.graphics.getDimensions()
   center = {['width'] = screenWidth * 0.5, ['height'] = screenHeight * 0.5 }
-  --print("mouseX: "..love.mouse.getX(), "map: "..map(love.mouse.getX(), 0, screenWidth, 0, 255))
 end
+
 function love.keypressed(key, scancode, isrepeat)
   if key == 'right' then
-    bend = bend+deltaTime*20
+    if isrepeat then
+      bend = bend + deltaTime * 60
+    else
+      bend = bend + deltaTime * 10
+    end
+  elseif key == 'left' then
+    if isrepeat then
+      bend = bend - deltaTime * 60
+    else
+      bend = bend - deltaTime * 10
+    end
+  elseif key == 'up' then
+    if isrepeat then
+      growLenght = growLenght + deltaTime * 60
+    else
+      growLenght = growLenght + deltaTime * 10
+    end
+  elseif key == 'down' then
+    if isrepeat then
+      growLenght = growLenght - deltaTime * 60
+    else
+      growLenght = growLenght - deltaTime * 10
+    end
+  elseif key == 'space' then
+    growLenght = 100
+    bend = 0
+  elseif key == 'r' then
+    growLenght = math.random(1, 200)
+    bend = math.random(-100, 100)
   end
 end
 
+
 function love.draw()
-  branch(center.width, center.height + 250, 200, - 90, bend)
+  --love.graphics.line(fractalWidthStart, 0, fractalWidthStart, screenHeight)
+  --love.graphics.line(fractalWidthEnd, 0, fractalWidthEnd, screenHeight)
+  branch(center.width, center.height + 250, growLenght, - 90, bend, 0)
 end
